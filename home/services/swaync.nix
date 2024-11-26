59adcecd104607fdeedd6acf35927b004fafe098
Anthony Rodriguez <anthony@nezia.dev>
2024-11-26 14:41:46 +0100

    home/services/swaync: fix ConditionEnvironment not working correctly with niri

--- home/services/swaync.nix
+++ home/services/swaync.nix
@@ -1,5 +1,7 @@
-_: {
+{lib, ...}: {
   services.swaync = {
     enable = true;
   };
+  # systemd.user.services.swaync.Service.Environment = "WAYLAND_DISPLAY=wayland-1";
+  systemd.user.services.swaync.Unit.ConditionEnvironment = lib.mkForce "";
 }
