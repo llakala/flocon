5a42498391ede5bdd9512e7112df69684365a6af
Anthony Rodriguez <anthony@nezia.dev>
2024-12-13 16:59:37 +0100

    modules/theme: update fuzzel selection colors

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -145,10 +145,10 @@ in {
             colors = {
               background = "${scheme.palette.base01}f2";
               text = "${scheme.palette.base05}ff";
-              match = "${scheme.palette.base0D}ff";
+              match = "${scheme.palette.base0E}ff";
               selection = "${scheme.palette.base03}ff";
               selection-text = "${scheme.palette.base06}ff";
-              selection-match = "${scheme.palette.base0D}ff";
+              selection-match = "${scheme.palette.base0E}ff";
               border = "${scheme.palette.base0E}ff";
             };
           };
